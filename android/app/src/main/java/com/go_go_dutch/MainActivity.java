f5c18aced7521098e7eda63efc1be55bdae867f7
fcamel <fcamel@gmail.com>
2017-07-13 09:05:37 +0800

    Add the files generated by "react-native init go-go-dutch".

---
+++ android/app/src/main/java/com/go_go_dutch/MainActivity.java
@@ -0,0 +1,15 @@
+package com.go_go_dutch;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "go_go_dutch";
+    }
+}

a32ca58a53cae6f2bb82c74577531915d13452a3
fcamel <fcamel@gmail.com>
2017-07-24 20:27:23 +0800

    move *.js to src/.

--- index.android.js
+++ index.android.js
@@ -1,18 +1,5 @@
 'use strict';
 
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import index from './src/index';
 
-import { TripListScreen, TripContentScreen } from './trip';
-import { AddExpenseScreen, ExpenseDetailScreen } from './expense';
-
-
-const GoGoDutch = StackNavigator({
-  Home: { screen: TripListScreen, },
-  Trip: { screen: TripContentScreen, },
-  AddExpense: { screen: AddExpenseScreen, },
-  ExpenseDetail: { screen: ExpenseDetailScreen, },
-});
-
-AppRegistry.registerComponent('GoGoDutch', () => GoGoDutch);
+index();
---
+++ src/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+import { TripListScreen, TripContentScreen } from './trip';
+import { AddExpenseScreen, ExpenseDetailScreen } from './expense';
+
+
+export default function() {
+  const GoGoDutch = StackNavigator({
+    Home: { screen: TripListScreen, },
+    Trip: { screen: TripContentScreen, },
+    AddExpense: { screen: AddExpenseScreen, },
+    ExpenseDetail: { screen: ExpenseDetailScreen, },
+  });
+
+  AppRegistry.registerComponent('GoGoDutch', () => GoGoDutch);
+}
